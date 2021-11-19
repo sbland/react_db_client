9066e7f37c25fed37f46bc096e7b724af181499b
sam bland <sbland.co.uk@gmail.com>
2021-11-19 22:37:19 +0000

    Update toggle box component

--- react_db_client/components/form/form-components/toggle-box/toggle-box.composition.jsx
+++ react_db_client/components/form/form-components/toggle-box/toggle-box.composition.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import { ToggleBox } from './toggle-box';
+import { ToggleBoxRadioGroup } from './radio-group';
 
-export const BasicToggleBox = () => (
-  <ToggleBox text="hello from ToggleBox" />
-);
+export const defaultToggleBox = () => {
+  return (
+    <div className="sectionWrapper">
+      <ToggleBox />
+    </div>
+  );
+};
+
+export const radioGroup = () => {
+  return (
+    <ToggleBoxRadioGroup
+      selected={0}
+      onChange={(i, v) => console.log(i, v)}
+      allowDeselect
+    >
+      <ToggleBox id={0} text="0" />
+      <ToggleBox id={1} text="1" />
+      <ToggleBox id={2} text="2" />
+    </ToggleBoxRadioGroup>
+  );
+};
+
+export const radioGroupNoDeselect = () => {
+  return (
+    <ToggleBoxRadioGroup selected={0} onChange={(i, v) => console.log(i, v)}>
+      <ToggleBox id={0} text="0" />
+      <ToggleBox id={1} text="1" />
+      <ToggleBox id={2} text="2" />
+    </ToggleBoxRadioGroup>
+  );
+};
--- react_db_client/components/form/form-components/toggle-box/toggle-box.jsx
+++ react_db_client/components/form/form-components/toggle-box/toggle-box.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Emoji } from '@samnbuk/react_db_client.components.emoji';
+import '@samnbuk/react_db_client.constants.style';
 
-export function ToggleBox({ text }) {
+export const ToggleBox = ({ stateIn, id, text, onChange, width, disabled }) => {
+  const [state, setState] = useState(stateIn);
+  const handleClick = () => {
+    if (disabled) return;
+    if (onChange) {
+      onChange(!state, id);
+    } else {
+      setState(!state, id);
+    }
+  };
+
+  useEffect(() => {
+    setState(stateIn);
+  }, [stateIn]);
+
+  const styleOverride = text ? {} : { width };
+
   return (
-    <div>
-      {text}
+    <div className="toggleBoxWrap">
+      <button
+        type="button"
+        className={state ? 'button-two' : 'button-one'}
+        onClick={handleClick}
+        style={styleOverride}
+      >
+        {text || (state ? <Emoji emoj="✔️" /> : <Emoji emoj="x" />)}
+      </button>
     </div>
   );
-}
+};
+
+ToggleBox.propTypes = {
+  stateIn: PropTypes.bool,
+  text: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  width: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  disabled: PropTypes.bool,
+};
+
+ToggleBox.defaultProps = {
+  text: null,
+  stateIn: false,
+  width: '100%',
+  disabled: false,
+};
+
+
