830623f73ac2790aeb83a3aa4cf0db3d08741f63
sam bland <sbland.co.uk@gmail.com>
2021-11-24 07:36:41 +0000

    Update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,9 @@
 const baseConfig = require("@teambit/react/prettier/prettier.config");
 module.exports = {
   ...baseConfig,
+  "printWidth": 100,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "semi": true
 };
