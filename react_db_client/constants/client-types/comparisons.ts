81fc26d4242dc45b8a4439793e519ea9a9dd48c8
sam bland <sbland.co.uk@gmail.com>
2022-06-24 21:35:27 +0100

    Refactored comparisons

--- react_db_client/constants/client-types/comparisons.ts
+++ react_db_client/constants/client-types/comparisons.ts
@@ -1,56 +1,29 @@
-
-
-export enum StringComparisons {
-  equals= '=',
-  contains= 'contains',
-  regex= 'regex',
-  empty= 'empty',
-};
-
-export enum NumberComparisons {
-  greaterThan= '>',
-  lesserThan= '<',
-};
-
-export enum DateComparisons {
-  after= 'after',
-  before= 'before',
-};
-
-
-export const Comparisons = {
-  ...StringComparisons,
-  ...NumberComparisons,
-  ...DateComparisons,
-}
-export type Comparisons = typeof Comparisons;
-
-
 export const stringComparisons = {
   equals: '=',
   contains: 'contains',
   regex: 'regex',
   empty: 'empty',
-};
+} as const;
 
 export const numberComparisons = {
   greaterThan: '>',
   lesserThan: '<',
-};
+} as const;
 
 export const dateComparisons = {
   after: 'after',
   before: 'before',
-};
+} as const;
 
 export const generalComparisons = {
   equals: '=',
-}
+};
 
 export const comparisons = {
   ...generalComparisons,
   ...stringComparisons,
   ...numberComparisons,
   ...dateComparisons,
-};
+} as const;
 
+export type Comparisons = typeof comparisons[keyof typeof comparisons];
