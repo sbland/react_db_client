0f891c2260511a7cfc7c4967764d5f02f22d37e7
sam bland <sbland.co.uk@gmail.com>
2023-02-27 12:01:57 +0000

    Update dependencies

--- react_db_client/hooks/use-auto-hide-panel-hook/auto-hide-panel-hook.tsx
+++ react_db_client/hooks/use-auto-hide-panel-hook/auto-hide-panel-hook.tsx
@@ -2,25 +2,21 @@ import { useCallback, useEffect, useState } from 'react';
 
 let hidePanelTimeout: NodeJS.Timeout | null = null;
 
-/**
- *
- * @param {*} menuRef
- * @param {*} floating
- * @param {*} showPanelOverride
- * @param {*} HIDETIME
- * @returns [showPanel, setShowPanel]
- */
 export const useAutoHidePanel = (
   menuRef: React.RefObject<HTMLElement>,
-  floating: boolean,
-  showPanelOverride: boolean,
+  floating: boolean | undefined | null,
+  showPanelOverride: boolean | undefined | null,
   HIDETIME = 900
 ) => {
   const [showPanel, setShowPanel] = useState(() => showPanelOverride);
 
   const handleClickOutside = useCallback(
     (event) => {
-      if (showPanel && menuRef.current && !menuRef.current.contains(event.target)) {
+      if (
+        showPanel &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target)
+      ) {
         hidePanelTimeout = setTimeout(() => {
           setShowPanel(false);
           document.removeEventListener('mouseover', handleClickOutside);
